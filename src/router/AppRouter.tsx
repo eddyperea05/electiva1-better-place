75548534036215e6b71cd68a96cedcf757045aa8
AndresBerdu <andres700crac@gmail.com>
2025-10-16 14:21:40 -0500

    se comentaron las lineas del archivo AppRouter ya que hay componentes que no existen

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { NotFound } from "../ui/components/NotFound";
 import { OwenershipsPage } from "../ownerships/pages/OwnershipsPage";
-import { HomePage } from "../ownerships/pages/HomePage";
-import { AboutUsPage } from "../ownerships/pages/AboutUsPage";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/OwnershipsPage" element={<OwenershipsPage />} />
-        <Route path="/about-us" element={<AboutUsPage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<OwenershipsPage />} />
+        {/* <Route path="/about-us" element={<AboutUsPage />} /> */}
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
