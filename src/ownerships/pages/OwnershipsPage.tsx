98fb5d6eb412b9ead96e3ef8928a70a6d13e1cd0
AndresBerdu <andres700crac@gmail.com>
2025-10-16 14:20:03 -0500

    se modifico la carpeta pages y el archivo OwnershipsPage

--- src/ownerships/pages/OwnershipsPage.tsx
+++ src/ownerships/pages/OwnershipsPage.tsx
@@ -1,20 +1,24 @@
+//Json de las propiedades
+import houses from "../../json/houses.json";
+
+//import componentes
+import { CardPropertyComponent } from "../components/CardPropertyComponent";
+import { Filters } from "../filters/Filters";
+
 export const OwenershipsPage = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h2>Nombre de casa</h2>
-          <p>código</p>
-        </div>
-        <img src="" alt="" />
-        <div>
-          <h3>Nombre del arrendador</h3>
-          <p>precio</p>
-        </div>
-        <div>
-          
-        </div>
-      </div>
+    <div className="md:flex">
+      {/* contenedor filtros */}
+      <section>
+        <Filters />
+      </section>
+      <section className="flex flex-col justify-center items-center md:flex-row md:flex-wrap md:gap-10 mx-5">
+        {/* contenedor por cada tarjeta */}
+        {houses.map((house: any) => (
+          /* componente de la tarjeta de propiedad */
+          <CardPropertyComponent key={house.codigoCasa} property={house} />
+        ))}
+      </section>
     </div>
   );
 };
