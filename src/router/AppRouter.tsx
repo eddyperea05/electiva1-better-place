d299a0a2a454d8e7dccb165bd66fe7ecfa007f34
AndresBerdu <andres700crac@gmail.com>
2025-10-19 14:02:25 -0500

    se creo la nueva ruta de Detalle

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { NotFound } from "../ui/components/NotFound";
 import { OwenershipsPage } from "../ownerships/pages/OwnershipsPage";
+import { DetailPropertyPage } from "../detailProperty/pages/DetailPropertyPage";
 
 export const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ export const AppRouter = () => {
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/" element={<OwenershipsPage />} />
+        <Route path="/Detalle" element={<DetailPropertyPage />} />
         {/* <Route path="/about-us" element={<AboutUsPage />} /> */}
         <Route path="/*" element={<NotFound />} />
       </Routes>
