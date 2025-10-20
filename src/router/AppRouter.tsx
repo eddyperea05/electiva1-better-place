1d40cdafe00a8573860026da3aaf2d0874f33be2
AndresBerdu <andres700crac@gmail.com>
2025-10-19 19:15:30 -0500

    se modifico el AppRouter para poder visualizar las ventanas

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { NotFound } from "../ui/components/NotFound";
 import { OwenershipsPage } from "../ownerships/pages/OwnershipsPage";
-import { HomePage } from "../ownerships/pages/HomePage";
-import { AboutUsPage } from "../ownerships/pages/AboutUsPage";
+import { LoginPage } from "../authentication/pages/LoginPage";
+import { RegisterPage } from "../authentication/pages/RegisterPage";
+/* import { AboutUsPage } from "../ownerships/pages/AboutUsPage"; */
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/*  <Route path="/" element={<HomePage />} /> */}
         <Route path="/OwnershipsPage" element={<OwenershipsPage />} />
-        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        {/* <Route path="/about-us" element={<AboutUsPage />} /> */}
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
