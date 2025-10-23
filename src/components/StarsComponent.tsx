5a7fe579625e8efa6ce678388afd31ad55e28206
AndresBerdu <andres700crac@gmail.com>
2025-10-23 16:51:13 -0500

    se cambiaron estilos

--- src/components/StarsComponent.tsx
+++ src/components/StarsComponent.tsx
@@ -2,26 +2,29 @@
 import { FaStar, FaStarHalf } from "react-icons/fa";
 
 export const StarsComponent = ({ amoutStarts }: { amoutStarts: number }) => {
-
   return (
     <>
       {/* para validar si mostramos una estrella y media o no 
       verificamos si el Numbero es entero o no */}
       {Number.isInteger(amoutStarts) ? (
-        <div className="flex">
+        <div className="flex items-center">
           {/* creamos un array apartir de la cantidad de estrellas */}
           {Array.from({ length: amoutStarts }).map((_, index) => (
             <FaStar key={index} className="mr-1 text-xl text-[#2A1EFA]" />
           ))}
+          <p className="text-gray-500">{amoutStarts}</p>
         </div>
       ) : (
-        <div className="flex">
-          {/* hacemos lo mismo para las estrellas normales solo que siempre va redondear al número
+        <div className="flex items-center">
+          <div className="flex">
+            {/* hacemos lo mismo para las estrellas normales solo que siempre va redondear al número
           más bajo y le agregamos una estrella y media */}
-          {Array.from({ length: Math.floor(amoutStarts) }).map((_, index) => (
-            <FaStar key={index} className="mr-1 text-xl text-[#2A1EFA]"/>
-          ))}
-          <FaStarHalf className="text-xl text-[#2A1EFA]" />
+            {Array.from({ length: Math.floor(amoutStarts) }).map((_, index) => (
+              <FaStar key={index} className="mr-1 text-xl text-[#2A1EFA]" />
+            ))}
+            <FaStarHalf className="text-xl text-[#2A1EFA]" />
+          </div>
+          <p className="text-gray-500">{amoutStarts}</p>
         </div>
       )}
     </>
