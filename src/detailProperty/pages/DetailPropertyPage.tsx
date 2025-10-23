2f82a36553f74da857f2c17dff1858fa4c11238b
AndresBerdu <andres700crac@gmail.com>
2025-10-23 16:54:25 -0500

    se agrearon utilidades y se cambiaron estilos

--- src/detailProperty/pages/DetailPropertyPage.tsx
+++ src/detailProperty/pages/DetailPropertyPage.tsx
@@ -1,9 +1,6 @@
 //import de componentes
 import { FaBed, FaBath, FaCar, FaArrowLeft } from "react-icons/fa6";
 
-//import de la imagen de la propiedad
-import casa from "../../assets/images/casa_mickey_mouse.webp";
-
 //import del contexto de los datos de la propiedad
 import { useDetailContext } from "../hooks/useDataContext";
 
@@ -11,10 +8,11 @@ import { useDetailContext } from "../hooks/useDataContext";
 import { AddCommentComponent } from "../components/AddCommentComponent";
 import { CommentsComponent } from "../components/CommentsComponent";
 import { StarsComponent } from "../../components/StarsComponent";
-import { CalendarComponent } from "../components/CalendarComponent";
 
 //import react router
 import { useNavigate } from "react-router-dom";
+import { formatPrice } from "../../utils/formatPrice";
+import { getPropertyIcon } from "../../utils/getPropertyIcon";
 
 export const DetailPropertyPage = () => {
   const navigate = useNavigate();
@@ -27,24 +25,26 @@ export const DetailPropertyPage = () => {
       <div className="max-w-[1026px]">
         <button
           onClick={() => navigate("/")}
-          className="ml-5 mb-5 bg-[#2A1EFA] p-3 rounded-md hover:bg-[#261DCC] duration-150 cursor-pointer"
+          className="ml-5 mb-5 bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] p-3 rounded-md cursor-pointer"
         >
           <FaArrowLeft className="text-white text-[1rem]" />
         </button>
         <div className="flex mb-4 md:mx-5">
           {/* renderizado de imagen de la propiedad */}
           <img
-            className="max-w-[1026px] w-full rounded-sm"
-            src={casa}
-            alt={`casa de ${data.nombreArrendador}`}
+            className="max-w-[1026px] w-full h-120 rounded-sm"
+            src={data.img}
+            alt={`casa de ${data.arrendatario}`}
           />
         </div>
         <section className="mx-5">
           <div>
             {/* código de la propiedad */}
-            <h2 className="font-semibold text-[1.5rem]">{data.codigoCasa}</h2>
+            <h2 className="font-semibold text-[#721EFA] text-[1.5rem]">
+              {data.codigoCasa}
+            </h2>
             {/* nombre de la propiedad */}
-            <h1 className="capitalize font-bold text-[2.5rem] mb-5">
+            <h1 className="capitalize font-bold text-gray-700 text-[2.5rem] mb-5">
               {data.nombreCasa}
             </h1>
             {/* componente para mostrar las estrellas */}
@@ -53,8 +53,10 @@ export const DetailPropertyPage = () => {
             </div>
             {/* descripción de la propiedad */}
             <div className="mb-5">
-              <h2 className="capitalize text-[2rem] font-bold">descripción</h2>
-              <p>
+              <h2 className="capitalize text-[2rem] font-bold text-gray-700">
+                descripción
+              </h2>
+              <p className="text-gray-500">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel,
                 explicabo nesciunt. Officia fugit aliquid corporis ullam
                 veritatis! Quos placeat omnis molestias, itaque officiis
@@ -64,87 +66,98 @@ export const DetailPropertyPage = () => {
             </div>
           </div>
           {/* tipo de propiedad */}
-          <div className="md:hidden">
-            <div className="mb-5">
-              <h3 className="capitalize outline-1 outline-black inline-block py-2 px-3 rounded-full font-semibold text-[1rem] ">
-                {data.tipoPropiedad}
-              </h3>
+          <div className="md:hidden outline outline-gray-300 p-4 rounded-sm">
+            <div className="outline outline-gray-300 py-2 px-3 inline-block rounded-full mb-5">
+              <div className="flex">
+                <p className="mr-2">{getPropertyIcon(data.tipoPropiedad)}</p>
+                <h3 className="capitalize text-[#721EFA]   font-semibold text-[1rem] ">
+                  {data.tipoPropiedad}
+                </h3>
+              </div>
             </div>
             {/* nombre del arrendador */}
-            <h2 className="capitalize font-semibold mb-3">{`propietario: ${data.nombreArrendador}`}</h2>
+            <h2 className="capitalize text-gray-600 font-semibold mb-5">{`propietario: ${data.arrendatario.nombre}`}</h2>
 
             {/* precio de arrendamiento por día */}
-            <h2 className="capitalize font-semibold mb-3">{`arrendar a partir de $ ${new Intl.NumberFormat(
-              "es-CO"
-            ).format(data.precio)}`}</h2>
+            <h2 className="capitalize text-gray-600 font-semibold mb-5">{`arrendar a partir de ${formatPrice(
+              data.precio
+            )}`}</h2>
 
             {/* área de la propiedad */}
-            <h4 className="capitalize font-semibold mb-5">{`área de la propiedad ${new Intl.NumberFormat(
-              "es-CO"
-            ).format(data.metrosCuadrados)}m²`}</h4>
+            <h4 className="capitalize text-gray-600 font-semibold mb-5">{`área de la propiedad ${data.metrosCuadrados}m²`}</h4>
 
             {/* cantidad de habitaciones, baños y parqueaderos */}
-            <div className="flex justify-between items-center">
-              <div className="flex justify-center items-center text-[#2A1EFA]">
-                <FaBed className="mr-3" />
-                <h3 className="font-bold">{data.habitaciones} hab.</h3>
+            <div className="flex justify-between items-center mb-3">
+              <div className="flex justify-center items-center">
+                <FaBed className="mr-3 text-[#BA1EFA]" />
+                <h3 className="font-bold text-[#721EFA]">
+                  {data.habitaciones} hab.
+                </h3>
               </div>
-              <div className="flex justify-center items-center text-[#2A1EFA]">
-                <FaBath className="mr-3" />
-                <h3 className="font-bold">{data.baños} bañ.</h3>
+              <div className="flex justify-center items-center">
+                <FaBath className="mr-3 text-[#BA1EFA]" />
+                <h3 className="font-bold text-[#721EFA]">{data.baños} bañ.</h3>
               </div>
-              <div className="flex justify-center items-center text-[#2A1EFA]">
-                <FaCar className="mr-3" />
-                <h3 className="font-bold">{data.parqueaderos} par.</h3>
+              <div className="flex justify-center items-center">
+                <FaCar className="mr-3 text-[#BA1EFA]" />
+                <h3 className="font-bold text-[#721EFA]">
+                  {data.parqueaderos} par.
+                </h3>
               </div>
             </div>
+            <button className="capitalize py-2 font-bold w-full bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] text-white rounded-sm cursor-pointer">
+              arrendar propiedad
+            </button>
           </div>
         </section>
         <section className="mx-5">
           {/* componente para agregar comentarios */}
-          <AddCommentComponent codeHouse={data.codigoCasa}/>
+          <AddCommentComponent codeHouse={data.codigoCasa} />
 
           {/* componente para visualizar los componentes */}
           <CommentsComponent codeHouse={data.codigoCasa} />
-
-          <CalendarComponent />
         </section>
       </div>
 
       {/* esto es muy machetero y duplica código pero por ahora fue la solución que encontre para
             que sea responsive */}
-      <div className="md:block bg-black hidden sticky top-0 self-start md:w-[300px] p-5 mt-16 mr-5 rounded-sm">
-        <div className="mb-5">
-          <h3 className="capitalize outline-1 outline-[#2A1EFA] inline-block py-2 px-3 rounded-full font-semibold text-white text-[1rem] ">
-            {data.tipoPropiedad}
-          </h3>
+      <div className="outline outline-gray-300 hidden sticky top-0 self-start md:w-[300px] p-5 mt-16 mr-5 rounded-sm md:block shadow-lg">
+        <div className="outline outline-gray-300 py-2 px-3 inline-block rounded-full mb-5">
+          <div className="flex">
+            <p className="mr-2">{getPropertyIcon(data.tipoPropiedad)}</p>
+            <h3 className="capitalize text-[#721EFA]   font-semibold text-[1rem] ">
+              {data.tipoPropiedad}
+            </h3>
+          </div>
         </div>
         {/* nombre del arrendador */}
-        <h2 className="text-white capitalize font-semibold mb-3">{`propietario: ${data.nombreArrendador}`}</h2>
+        <h2 className="capitalize text-gray-600 font-semibold mb-5">{`propietario: ${data.arrendatario.nombre}`}</h2>
 
         {/* precio de arrendamiento por día */}
-        <h2 className="text-white capitalize font-semibold mb-3">{`arrendar a partir de $ ${new Intl.NumberFormat(
-          "es-CO"
-        ).format(data.precio)}`}</h2>
+        <h2 className="capitalize text-gray-600 font-semibold mb-5">{`arrendar a partir de ${formatPrice(
+          data.precio
+        )}`}</h2>
 
         {/* área de la propiedad */}
-        <h4 className="text-white capitalize font-semibold mb-5">{`área de la propiedad ${new Intl.NumberFormat(
-          "es-CO"
-        ).format(data.metrosCuadrados)}m²`}</h4>
+        <h4 className="capitalize text-gray-600 font-semibold mb-5">{`área de la propiedad ${data.metrosCuadrados}m²`}</h4>
 
         {/* cantidad de habitaciones, baños y parqueaderos */}
         <div className="flex justify-between items-center mb-5">
-          <div className="flex justify-center items-center text-white">
-            <FaBed className="mr-3" />
-            <h3 className="font-bold">{data.habitaciones} hab.</h3>
+          <div className="flex justify-center items-center">
+            <FaBed className="mr-3 text-[#BA1EFA]" />
+            <h3 className="font-bold text-[#721EFA]">
+              {data.habitaciones} hab.
+            </h3>
           </div>
-          <div className="flex justify-center items-center text-white">
-            <FaBath className="mr-3" />
-            <h3 className="font-bold">{data.baños} bañ.</h3>
+          <div className="flex justify-center items-center">
+            <FaBath className="mr-3 text-[#BA1EFA]" />
+            <h3 className="font-bold text-[#721EFA]">{data.baños} bañ.</h3>
           </div>
-          <div className="flex justify-center items-center text-white">
-            <FaCar className="mr-3" />
-            <h3 className="font-bold">{data.parqueaderos} par.</h3>
+          <div className="flex justify-center items-center">
+            <FaCar className="mr-3 text-[#BA1EFA]" />
+            <h3 className="font-bold text-[#721EFA]">
+              {data.parqueaderos} par.
+            </h3>
           </div>
         </div>
         <button className="capitalize py-2 font-bold w-full bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] text-white rounded-sm cursor-pointer">
