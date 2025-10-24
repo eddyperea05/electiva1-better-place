b488ccd103cd9e2362a3aea225d651c997380984
AndresBerdu <andres700crac@gmail.com>
2025-10-24 08:47:55 -0500

    se cambiaron las rutas

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,15 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import { NotFound } from "../ui/components/NotFound";
-import { OwenershipsPage } from "../ownerships/pages/OwnershipsPage";
-import { HomePage } from "../ownerships/pages/HomePage";
-import { AboutUsPage } from "../ownerships/pages/AboutUsPage";
+/* import { HomePage } from "../ownerships/pages/HomePage"; */
+import { PropertiesPage } from "../properties/pages/PropertiesPage";
+import { AboutUsPage } from "../aboutUS/page/AboutUsPage";
+import { DetailPropertyPage } from "../detailProperty/pages/DetailPropertyPage";
+import { LoginPage } from "../authentication/pages/LoginPage";
+import { RegisterPage } from "../authentication/pages/RegisterPage";
+import { MyLeasesPage } from "../myLeases/pages/MyLeasesPage";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/OwnershipsPage" element={<OwenershipsPage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/properties" element={<PropertiesPage />} />
+        <Route path="/MyLeases" element={<MyLeasesPage />} />
+        <Route path="/Detail" element={<DetailPropertyPage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
