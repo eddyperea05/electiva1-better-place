ea12339f39890e8b6fe2f418d228155acb340bb4
AndresBerdu <andres700crac@gmail.com>
2025-11-07 09:25:17 -0500

    se creo el archivo para las runas protegidas

---
+++ src/router/GeneralRoutes.tsx
@@ -0,0 +1,39 @@
+//Imports de react-router
+import { Route, Routes } from "react-router-dom";
+
+//Imports de componentes
+import { Navbar } from "../ui/components/Navbar";
+import { PropertiesPage } from "../properties/pages/PropertiesPage";
+import { AboutUsPage } from "../aboutUS/page/AboutUsPage";
+import { DetailPropertyPage } from "../detailProperty/pages/DetailPropertyPage";
+import { MyLeasesPage } from "../myLeases/pages/MyLeasesPage";
+import { MyProperties } from "../ownerships/pages/MyProperties";
+import { PropertyDetailPage } from "../ownerships/pages/PropertyDetailPage";
+import { NotFound } from "../ui/components/NotFound";
+import { Footer } from "../ui/components/Footer";
+import { PrivatePolicityMyLessesRouter } from "./PrivatePolicityMyLessesRouter";
+
+export const GeneralRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/properties" element={<PropertiesPage />} />
+        <Route
+          path="/myLeases"
+          element={
+            <PrivatePolicityMyLessesRouter>
+              <MyLeasesPage />
+            </PrivatePolicityMyLessesRouter>
+          }
+        />
+        <Route path="/myProperties" element={<MyProperties />} />
+        <Route path="/detail" element={<DetailPropertyPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/propietario/:id" element={<PropertyDetailPage />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
