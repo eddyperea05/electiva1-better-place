1a1f6706823e02aef4a084f5222d6531b14b4cdc
AndresBerdu <andres700crac@gmail.com>
2025-11-07 09:34:05 -0500

    se añadieron las funciones y los objetos para usar el firebase

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+//Credenciales de direbase
 const firebaseConfig = {
   apiKey: "AIzaSyCOt2mFzEqwTocCZrKAMix_XtiKA5p25zE",
   authDomain: "electiva-1-better-place.firebaseapp.com",
@@ -10,9 +12,20 @@ const firebaseConfig = {
   appId: "1:36310906477:web:4898b3690fabea3820f164"
 };
 
+//Función para inicializar la app de firebase
 const app = initializeApp(firebaseConfig);
+
+//Función para llamar a la base de datos de firebase
 const db = getFirestore(app);
+
+//Función para llamar la authenticacion por Email y password
+const auth = getAuth(app);
+
+//Objeto para autenticar con google
+const googleProvider = new GoogleAuthProvider();
 
 export {
     db,
+    auth,
+    googleProvider,
 }
