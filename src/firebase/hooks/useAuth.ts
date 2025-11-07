5e08bd517f4444bc5bbcc6134675b8f056df9d2f
AndresBerdu <andres700crac@gmail.com>
2025-11-07 09:33:14 -0500

    se creo un hook para saber si se autentico el usuario

---
+++ src/firebase/hooks/useAuth.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, type User } from "firebase/auth";
+import { auth } from "../firebase";
+
+export const useAuth = () => {
+
+  //Inicializamos con el usuario, en caso de que ya exista una sección
+  const [user, setUser] = useState<User | null>(auth.currentUser);
+
+  useEffect( () => {
+    //Con esta función podemos saber si el usuario se logueo o cerro sección
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return { user };
+};
