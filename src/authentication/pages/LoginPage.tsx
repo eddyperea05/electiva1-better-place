a9077f9b33f9b9263e99c37607c6feab1f166bf7
AndresBerdu <andres700crac@gmail.com>
2025-11-07 09:40:53 -0500

    se modifico el login y el registro para usar firebase, yup y formik

--- src/authentication/pages/LoginPage.tsx
+++ src/authentication/pages/LoginPage.tsx
@@ -1,9 +1,100 @@
+import { useState } from "react";
+
+//imports de iconos
 import { FcGoogle } from "react-icons/fc";
+
+//imports de react-router
 import { useNavigate } from "react-router-dom";
+
+//imports de tipos
+import type { UserDataLogin } from "../types/userDataType";
+
+import {
+  loginUser,
+  loginWithGoogle,
+} from "../../firebase/functions/functionsFirebase";
+import { userDataContext } from "../hooks/userDataContext";
+import { handleLoginErrors } from "../errors/errorsValidations";
+import { LoadingModal } from "../../ui/modals/LoadingModal";
 
 export const LoginPage = () => {
   /* hook para la navegacion */
   const navigate = useNavigate();
+
+  //Se envia el los datos del usuario
+  const { setUserData } = userDataContext();
+
+  //Hook para el login del usuario
+  const [userDataLogin, setUserDataLogin] = useState<UserDataLogin>({
+    email: "",
+    password: "",
+  });
+
+  //Hook para manejar los errores que provienen del firebase
+  const [errorsLogin, setErrorsLogin] = useState<String | undefined>("");
+
+  //Hook para saber si estan cargando los datos
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  //Función para capturar el valor y el nombre del campo
+  const onChangeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserDataLogin((prevdata) => ({
+      ...prevdata,
+      [name]: value.trim(),
+    }));
+  };
+
+  //Función para ingresar con firebase
+  const handleAuth = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    //Función para evitar la recarga del navegador
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    //Usamos la función para validar el logeo del usuario
+    const dataUser = await loginUser(
+      userDataLogin.email,
+      userDataLogin.password
+    );
+
+    //Validamos que no haya ninigún error
+    if (!dataUser.ok) {
+      //Enviamos un el tipo de error y lo imprimimos en el formulario
+      const error = handleLoginErrors(dataUser.error);
+      setErrorsLogin(error);
+
+      setIsLoading(false);
+
+      return false;
+    }
+
+    //Enviamos los datos al contexto
+    setUserData(dataUser.userDataGetting);
+
+    setIsLoading(false);
+
+    //redireción a propiedades
+    navigate("/properties");
+  };
+
+  const handleGoogleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const result = await loginWithGoogle();
+
+    if (!result.ok) {
+      setIsLoading(false);
+      const error = handleLoginErrors(result.error);
+      setErrorsLogin(error);
+      return;
+    }
+
+    setIsLoading(false);
+    setUserData(result.userData);
+    navigate("/properties");
+  };
 
   return (
     <div className="w-100% h-screen">
@@ -20,6 +111,7 @@ export const LoginPage = () => {
             <div className="flex flex-col">
               {/* input para el email */}
               <input
+                onChange={onChangeLogin}
                 name="email"
                 className="border mt-5 border-[#797979] p-2 rounded-md"
                 type="email"
@@ -27,17 +119,26 @@ export const LoginPage = () => {
               />
               {/* input para la contraseña */}
               <input
+                onChange={onChangeLogin}
                 name="password"
                 className="border mt-5 border-[#797979] p-2 rounded-md"
                 type="password"
                 placeholder="contraseña"
               />
+
+              {/* Imprimimos el error si no esta vació */}
+              {errorsLogin && (
+                <div className="w-full flex items-center justify-center bg-red-900 mt-6 py-2 rounded-sm">
+                  <span className="text-red-400">{errorsLogin}</span>
+                </div>
+              )}
+
               {/* bóton para iniciar sección */}
               <button
-                onClick={() => navigate("/")}
-                className="capitalize w-100% bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] rounded-md py-2.5 block mt-5 cursor-pointer hover:transition-colors duration-200"
+                onClick={handleAuth}
+                className="capitalize w-100% bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] rounded-md py-2.5 mt-5 block cursor-pointer hover:transition-colors duration-200"
               >
-                <strong>iniciar Sesion</strong>
+                <strong>iniciar sección</strong>
               </button>
               <div className="my-5">
                 {/* botón para redirigir al registro */}
@@ -50,17 +151,21 @@ export const LoginPage = () => {
               </div>
               <hr />
               {/* botón para iniciar sección con Google */}
-              <button className="mt-5 p-2 font-semibold bg-white text-black rounded-md flex items-center justify-center cursor-pointer">
+              <button
+                onClick={handleGoogleLogin}
+                className="capitalize mt-5 p-2 font-semibold bg-white text-black rounded-md flex items-center justify-center cursor-pointer"
+              >
                 <FcGoogle className="text-[1.5 rem] mr-2" />
-                Login with Google
+                ingresa con google
               </button>
               {/* botón para reestablecer la contraseña */}
-              <button className="text-center mt-2 cursor-pointer">
-                Do you forget your password?
+              <button className="capitalize text-center mt-2 cursor-pointer">
+                ¿olvidastes tú contraseña?
               </button>
             </div>
           </div>
         </form>
+        {isLoading ? <LoadingModal /> : ""}
       </div>
     </div>
   );
--- src/authentication/pages/RegisterPage.tsx
+++ src/authentication/pages/RegisterPage.tsx
@@ -1,89 +1,237 @@
+import { useState } from "react";
+
+//import iconos
 import { FcGoogle } from "react-icons/fc";
+
+//import react-router
 import { useNavigate } from "react-router-dom";
+
+//imports de los tipos
+import type { UserDataRegister } from "../types/userDataType";
+
+//import del context de los datos del usuario
+import { userDataContext } from "../hooks/userDataContext";
+
+//imports de la función de registro de firebase
+import {
+  loginUser,
+  loginWithGoogle,
+  registerUser,
+} from "../../firebase/functions/functionsFirebase";
+
+//imports de formik
+import { ErrorMessage, Field, Form, Formik } from "formik";
+
+//imports de yup
+import { getRegisterSchema } from "../schemas/registerSchema";
+
+//imports de componentes
+import { LoadingModal } from "../../ui/modals/LoadingModal";
 
 export const RegisterPage = () => {
   /* hook para la navegacion */
   const navigate = useNavigate();
 
+  //Contexto para enviar datos al usuario
+  const { setUserData } = userDataContext();
+
+  //Hook para saber si estan cargando los datos
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  //Inicializamos los valores por defecto en un objeto
+  const initialValuesUserRegister: UserDataRegister = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    numberPhone: "",
+    isOwner: false,
+    password: "",
+  };
+
+  //Funcion para crear un nuevo usuario en Firevase
+  const handleRegister = async (value: typeof initialValuesUserRegister) => {
+    setIsLoading(true);
+
+    //Registramos el usuario con los datos ingresados al input
+    const dataUserRegister = await registerUser(
+      value.firstName,
+      value.lastName,
+      value.userName,
+      value.email,
+      value.numberPhone,
+      value.password
+    );
+
+    //Validamos que no haya ninigún error
+    if (!dataUserRegister.ok) {
+      //enviamos error al desarrollador
+      console.error(dataUserRegister.error);
+
+      setIsLoading(false);
+
+      //Retornamos un null para que no pueda seguir el flujo
+      return null;
+    }
+
+    //Autenticamos el usuario de una vez
+    const userLogin = await loginUser(value.email, value.password);
+
+    //Verificamos si el usario si fue autenticado correctamente
+    if (!userLogin.ok) {
+      setIsLoading(false);
+      console.error("No se pudo autenticar al usuario");
+    }
+
+    //Le enviamos los datos del usuario al contexto
+    setUserData(userLogin.userDataGetting);
+
+    setIsLoading(false);
+
+    //Cuando el usuario se registra lo enviamos de nuvo al login para que haga login
+    navigate("/properties");
+  };
+
+  //Función para registrar y entrar con cuenta de google
+  const handleGoogleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const result = await loginWithGoogle();
+
+    if (!result.ok) {
+      setIsLoading(false);
+      return;
+    }
+
+    setIsLoading(false);
+    setUserData(result.userData);
+
+    navigate("/properties");
+  };
+
   return (
     <div className="w-100% h-full">
       <div className="flex items-center justify-center h-full">
         {/* formulario principal para el registro */}
-        <form>
-          <div className="capitalize bg-black text-white w-100 rounded-md p-6 my-10">
-            <h1 className="text-2xl">
-              <strong>bienvenido a better place</strong>
-            </h1>
-            <h2 className="mt-2">
-              La mejor aplicación para arrendar propiedades
-            </h2>
-            <div className="flex flex-col">
-              <div className="flex mt-5 justify-between">
-                <div>
+        <Formik
+          initialValues={initialValuesUserRegister}
+          validationSchema={getRegisterSchema}
+          onSubmit={handleRegister}
+        >
+          {/* Usamos el form que nos provee formik */}
+          <Form>
+            <div className="capitalize bg-black text-white w-100 rounded-md p-6 my-10">
+              <h1 className="text-2xl">
+                <strong>bienvenido a better place</strong>
+              </h1>
+              <h2 className="mt-2">
+                La mejor aplicación para arrendar propiedades
+              </h2>
+              <div className="flex flex-col">
+                <div className="flex mt-5 justify-between">
+                  <div>
                     {/* input para los nombres */}
-                  <input
-                    name="firstName"
-                    className="border-1 border-[#797979] p-2 rounded-md w-40"
-                    type="text"
-                    placeholder="Nombres"
-                  />
-                </div>
-                <div>
+                    <Field
+                      name="firstName"
+                      className="border border-[#797979] p-2 rounded-md w-40"
+                      placeholder="Nombres"
+                    />
+                    <ErrorMessage
+                      name="firstName"
+                      component="p"
+                      className="mt-1.5 text-red-600"
+                    />
+                  </div>
+                  <div>
                     {/* input para los apellidos */}
-                  <input
-                    name="lastName"
-                    className="border-1 border-[#797979] p-2 rounded-md w-40"
-                    type="text"
-                    placeholder="Apellidos"
-                  />
+                    <Field
+                      name="lastName"
+                      className="border border-[#797979] p-2 rounded-md w-40"
+                      placeholder="Apellidos"
+                    />
+                    <ErrorMessage
+                      name="lastName"
+                      component="p"
+                      className="mt-1.5 text-red-600"
+                    />
+                  </div>
                 </div>
-              </div>
-              {/* input para el email */}
-              <input
-                name="email"
-                className="border-1 mt-5 border-[#797979] p-2 rounded-md"
-                type="email"
-                placeholder="Email"
-              />
-              {/* input para el número de teléfono */}
-              <input
-                name="numberPhone"
-                className="border-1 mt-5 border-[#797979] p-2 rounded-md"
-                type="text"
-                placeholder="Número de teléfono"
-              />
-              {/* input para la contraseña */}
-              <input
-                name="password"
-                className="border-1 mt-5 border-[#797979] p-2 rounded-md"
-                type="password"
-                placeholder="Contraseña"
-              />
-              {/* botón para registrar el usuario */}
-              <button
-                type="submit"
-                onClick={() => navigate("/login")}
-                className="capitalize w-100% bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] rounded-md py-2.5 block mt-5 cursor-pointer hover:transition-colors duration-200"
-              >
-                <strong>registrarse</strong>
-              </button>
-              <div className="my-5">
-                {/* botón para redirigir al la vista de login */}
+                {/* input para el email */}
+                <Field
+                  name="email"
+                  type="email"
+                  className="border mt-5 border-[#797979] p-2 rounded-md"
+                  placeholder="Email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="p"
+                  className="mt-1.5 text-red-600"
+                />
+                {/* input para el email */}
+                <Field
+                  name="userName"
+                  className="border mt-5 border-[#797979] p-2 rounded-md"
+                  placeholder="Nombre de usuario"
+                />
+                <ErrorMessage
+                  name="userName"
+                  component="p"
+                  className="mt-1.5 text-red-600"
+                />
+                {/* input para el número de teléfono */}
+                <Field
+                  name="numberPhone"
+                  className="border mt-5 border-[#797979] p-2 rounded-md"
+                  placeholder="Número de teléfono"
+                />
+                <ErrorMessage
+                  name="numberPhone"
+                  component="p"
+                  className="mt-1.5 text-red-600"
+                />
+                {/* input para la contraseña */}
+                <Field
+                  name="password"
+                  type="password"
+                  className="border mt-5 border-[#797979] p-2 rounded-md"
+                  placeholder="Contraseña"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="p"
+                  className="mt-1.5 text-red-600"
+                />
+                {/* botón para registrar el usuario */}
                 <button
-                  onClick={() => navigate("/")}
-                  className="border-b-1 cursor-pointer"
+                  type="submit"
+                  className="capitalize w-100% bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] rounded-md py-2.5 block mt-5 cursor-pointer hover:transition-colors duration-200"
                 >
-                  ¿Ya tienes una cuenta creada?
+                  <strong>registrarse</strong>
+                </button>
+                <div className="my-5">
+                  {/* botón para redirigir al la vista de login */}
+                  <button
+                    onClick={() => navigate("/login")}
+                    className="border-b cursor-pointer"
+                  >
+                    ¿Ya tienes una cuenta creada?
+                  </button>
+                </div>
+                {/* botón para registrarse con google */}
+                <button
+                  onClick={handleGoogleLogin}
+                  className="capitalize bg-white font-semibold text-black p-2 rounded-md flex items-center justify-center cursor-pointer"
+                >
+                  <FcGoogle className="text-[1.5 rem] mr-2" />
+                  registrate con google
                 </button>
               </div>
-              {/* botón para registrarse con google */}
-              <button className="capitalize bg-white font-semibold text-black p-2 rounded-md flex items-center justify-center cursor-pointer">
-                <FcGoogle className="text-[1.5 rem] mr-2" />
-                registrate con google
-              </button>
             </div>
-          </div>
-        </form>
+          </Form>
+        </Formik>
+        {isLoading ? <LoadingModal /> : ""}
       </div>
     </div>
   );
