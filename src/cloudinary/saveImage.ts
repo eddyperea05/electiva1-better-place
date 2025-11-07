e679b03031cb5297da1687beb4ce5dc01be0b4fa
AndresBerdu <andres700crac@gmail.com>
2025-11-07 09:35:01 -0500

    se crearon las funciones para poder cargar las imagenes

---
+++ src/cloudinary/saveImage.ts
@@ -0,0 +1,47 @@
+//Import para cargar la imagen por defecto para los nuevos usuarios
+import defaultImage from "../assets/images/imageProfile.webp";
+
+//Keys para el cloudinary
+const upload_preset = "betterPlace";
+const cloud_name =  "dyr30w7ht"
+
+//Función para guardar una imagen por defecto en el registro
+export const handleDeafaultImage = async () => {
+
+    //Promesa para convertir mi archivo en un blob
+    const file = await fetch(defaultImage)
+      .then((res) => res.blob());
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", upload_preset); 
+
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const data = await res.json();
+    return data.secure_url; 
+};
+
+//Función para guardar imagenes que carga el usuario
+export const handleUploadImage = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", upload_preset);
+
+  const res = await fetch(
+    `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  const data = await res.json();
+  return data.secure_url;
+};
