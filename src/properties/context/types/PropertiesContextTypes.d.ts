4ef1ba6aa23105c96ea0af6e984f0234631e9012
AndresBerdu <andres700crac@gmail.com>
2025-11-13 09:09:22 -0500

    se modifico los tipos del contexto de las propiedades

--- src/properties/context/types/PropertiesContextTypes.d.ts
+++ src/properties/context/types/PropertiesContextTypes.d.ts
@@ -1,4 +1,22 @@
-export interface FastFilter {
-  typeFastFilter: string;
-  setFastFilter: React.Dispatch<React.SetStateAction<string>>
+//Con este tipo vamos a poder aplicar varios filtros a la vez
+type Filter = {
+  typeProperties?: string[];
+  sortBy?: string;
+  amountStuffs: {
+    habitaciones: number | string;
+    baños: number | string;
+    parqueaderos: number | string;
+  };
+  budgetRange?: {
+    min?: number;
+    max?: number;
+  };
+  metersRange?: {
+    min?: number;
+    max?: number;
+  };
+};
+export interface FilterInterface {
+  typeFastFilter: Filter;
+  setFastFilter: React.Dispatch<React.SetStateAction<Filter>>;
 }
