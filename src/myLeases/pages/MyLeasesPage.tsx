ba5b9767f76ec175d4aff6a5ef203ee397261d31
AndresBerdu <andres700crac@gmail.com>
2025-11-14 12:02:37 -0500

    se modifico la pagina de MyLeasesPage

--- src/myLeases/pages/MyLeasesPage.tsx
+++ src/myLeases/pages/MyLeasesPage.tsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
-import propiedades from "../../json/propiedades.json";
+import { useEffect, useState } from "react";
 import { PropertyModal } from "../components/PropertyModal";
 import { PropertyCard } from "../components/PropertyCard";
-
+import { userDataContext } from "../../authentication/hooks/userDataContext";
+import { getProperties } from "../../firebase/functions/functionsPropertiesFirebase";
+ 
 export const MyLeasesPage = () => {
   const [selectedProperty, setSelectedProperty] = useState(null);
-
-
-  // Filtrar solo propiedades arrendadas
-  const propiedadesArrendadas = propiedades.filter(
-    (prop) => prop.estado === "arrendada"
-  );
-
+  const { userData } = userDataContext();
+  const [propertiesUser, setpropertiesUser] = useState<any>([]);
+ 
+  useEffect(() => {
+    const fetchProperties = async () => {
+      const data = await getProperties();
+      setpropertiesUser(data?.filter((x) => x.Arrendador == userData.id));
+    };
+    fetchProperties();
+   
+  }, [propertiesUser]);
+ 
   return (
     <div className="min-h-screen bg-white">
-
       <section className="pt-20 pb-16 px-6 bg-gradient-to-br from-purple-50 to-blue-50">
         <div className="max-w-7xl mx-auto">
           <h1 className="text-5xl md:text-6xl font-bold mb-4 text-black">
@@ -25,33 +30,30 @@ export const MyLeasesPage = () => {
           </p>
         </div>
       </section>
-
+ 
       <section className="py-16 px-6">
         <div className="max-w-7xl mx-auto">
           <div className="mb-8">
             <h2 className="text-2xl font-bold text-black">
-              {propiedadesArrendadas.length}{" "}
-              {propiedadesArrendadas.length === 1
+              {propertiesUser.length}{" "}
+              {propertiesUser.length === 1
                 ? "Propiedad Arrendada"
                 : "Propiedades Arrendadas"}
             </h2>
           </div>
-
-
-                
+ 
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {propiedadesArrendadas.map((propiedad, index) => (
+            {propertiesUser.map((propertiesUser, index) => (
               <PropertyCard
                 key={index}
-                propiedad={propiedad}
+                propiedad={propertiesUser}
                 onClick={setSelectedProperty}
               />
             ))}
           </div>
         </div>
       </section>
-
-
+ 
       {selectedProperty && (
         <PropertyModal
           propiedad={selectedProperty}
