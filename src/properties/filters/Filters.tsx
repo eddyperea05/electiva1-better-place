732006c04b256bf9d22ad7de2bf1d529c1358c2c
AndresBerdu <andres700crac@gmail.com>
2025-11-14 12:03:05 -0500

    se modifico la página donde se muestran los filtros

--- src/properties/filters/Filters.tsx
+++ src/properties/filters/Filters.tsx
@@ -5,7 +5,7 @@ import { FastFilterComponent } from "./components/FastFilterComponent";
 export const Filters = () => {
   return (
     <div className="flex py-5 flex-col justify-center items-center md:items-start md:flex-row md:flex-wrap mx-7 md:mx-0 md:w-80 md:h-full z-80">
-      <div className="flex relative justify-between mx-5 w-full md:justify-center md:flex-col md:h-full md:static">
+      <div className="flex relative mx-5 w-full md:flex-col md:h-full md:static">
         {/* filtros rápidos */}
         <FastFilterComponent />
 
