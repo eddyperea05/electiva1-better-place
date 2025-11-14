cbfb8bdb42af432e5449543889b67011e1f1f265
AndresBerdu <andres700crac@gmail.com>
2025-11-14 12:05:02 -0500

    se modifico el modal de loas arrendamientos

--- src/ui/modals/propertyLeasedSuccessModal.tsx
+++ src/ui/modals/propertyLeasedSuccessModal.tsx
@@ -1,39 +1,50 @@
-import { useState } from "react";
-
-//import icons
-import { IoClose } from "react-icons/io5";
+import { useEffect, useState } from "react";
 
 //Import firebase function
 import { leasePropertyByCode } from "../../firebase/functions/functionsPropertiesFirebase";
 
 //Import del contexto de la propiedad
 import { useDetailContext } from "../../detailProperty/hooks/useDataContext";
+import { PropertyLeasedSuccessfully } from "./PropertyLeasedSuccesfully";
+import { userDataContext } from "../../authentication/hooks/userDataContext";
 
 export const PropertyLeasedSuccessModal = () => {
-
   const { data } = useDetailContext();
-
-  console.log(data)
+  const { userData } = userDataContext();
 
   //Hook para abrir el modal
   const [isOpenModal, setIsOpenModal] = useState<Boolean>(false);
-  const [leaseDate, setLeaseDate] = useState<Date | null>(null);
+  const [error, setError] = useState<String | null>("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [isLeased, setIsLeased] = useState(false);
+
+  useEffect(() => {
+    if (data.status === "arrendada") {
+      setIsLeased(true);
+    }
+  }, [data]);
 
   //Función apra abrir y cerrar el modal
   const handleClickOpenAndCloseModal = () => {
     setIsOpenModal(!isOpenModal);
-    /* Hacemos un cambio en el estilo directo para que no se pueda hacer 
-    el scroll de las ventanas anteriores */
-    document.body.style.overflow = !isOpenModal ? "hidden" : "auto";
+    setError("");
   };
 
+  //Función para arrendar
   const handleLeaseProperty = async () => {
-    if (!leaseDate) return alert("Selecciona una fecha de arrendamiento");
+    if (data.status === "arrendada") return;
 
-
+    //Intentamos arrendar la propiedad en firebase
     try {
-      await leasePropertyByCode(data.code, leaseDate);
-      handleClickOpenAndCloseModal();
+      const status = await leasePropertyByCode(data.code, userData.id);
+      if (status.ok) {
+        handleClickOpenAndCloseModal();
+        setShowSuccessMessage(true);
+        setIsLeased(true);
+        setError("");
+      } else {
+        setError(status.error);
+      }
     } catch (error) {
       console.error("Sucedio un error al tratar de guardar: ", error);
     }
@@ -41,55 +52,24 @@ export const PropertyLeasedSuccessModal = () => {
 
   return (
     <>
+      {showSuccessMessage && <PropertyLeasedSuccessfully />}
+
+      {error && (
+        <div className="w-full flex items-center justify-center bg-red-900 mt-6 py-2 rounded-sm">
+          <span className="text-red-400">{error}</span>
+        </div>
+      )}
       <button
-        onClick={handleClickOpenAndCloseModal}
-        className="capitalize py-2 font-bold w-full bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] text-white rounded-sm cursor-pointer"
+        onClick={handleLeaseProperty}
+        disabled={isLeased}
+        className={`capitalize py-2 font-bold w-full text-white rounded-sm cursor-pointer ${
+          isLeased
+            ? "bg-gray-400 cursor-not-allowed"
+            : "bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA]"
+        }`}
       >
         arrendar propiedad
       </button>
-
-      {isOpenModal && (
-        <div
-          onClick={handleClickOpenAndCloseModal}
-          className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50"
-        >
-          {/* opacidad para ael fonto */}
-          <div className="absolute inset-0 bg-black opacity-40"></div>
-
-          {/* Utilizo la función stopPropagation para que no se cierre la ventana */}
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className="relative w-110 bg-white p-6 rounded shadow-lg z-50"
-          >
-            <div className="flex items-center justify-between mb-4">
-              <h3 className="capitalize text-[#2A1EFA] font-semibold mb-3">
-                numero de días ha arrendar
-              </h3>
-              {/* botón para cerrar el modal */}
-              <button
-                onClick={handleClickOpenAndCloseModal}
-                className="bg-[#2A1EFA] p-2 rounded-sm cursor-pointer"
-              >
-                <IoClose className="text-white" />
-              </button>
-            </div>
-            <input
-              className="w-full p-2 outline outline-gray-300 mb-3 rounded-sm"
-              type="date"
-              onChange={(e) => setLeaseDate(new Date(e.target.value))}
-              placeholder="Número de días..."
-            />
-
-            {/* Botón para arrendar la propiedad */}
-            <button
-              onClick={handleLeaseProperty}
-              className="capitalize bg-linear-to-r from-[#2A1EFA] to-[#BA1EFA] py-2 text-white font-bold rounded-sm cursor-pointer w-full"
-            >
-              arrendar
-            </button>
-          </div>
-        </div>
-      )}
     </>
   );
 };
