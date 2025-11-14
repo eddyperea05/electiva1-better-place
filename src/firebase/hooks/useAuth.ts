f463de6b713b0339f2381ad6c7c44272f10f9995
AndresBerdu <andres700crac@gmail.com>
2025-11-13 22:34:08 -0500

    se modifico el customHook para el usuario

--- src/firebase/hooks/useAuth.ts
+++ src/firebase/hooks/useAuth.ts
@@ -6,15 +6,17 @@ export const useAuth = () => {
 
   //Inicializamos con el usuario, en caso de que ya exista una sección
   const [user, setUser] = useState<User | null>(auth.currentUser);
+  const [loading, setLoading] = useState(true);
 
   useEffect( () => {
     //Con esta función podemos saber si el usuario se logueo o cerro sección
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  return { user };
+  return { user, loading  };
 };
