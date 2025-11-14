ad3f80132b21db01bac5092cdfcc6c2e0acce23a
AndresBerdu <andres700crac@gmail.com>
2025-11-13 22:30:54 -0500

    se modifico el proveedor del detalle

--- src/detailProperty/context/DetailProvider.tsx
+++ src/detailProperty/context/DetailProvider.tsx
@@ -1,13 +1,21 @@
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { DetailContext } from "./DetailContext";
 
 //import tipos
 import type { PropiedadInterface } from "../../properties/types/propertyType";
 
 export const DetailProvider = ({ children }: { children: ReactNode }) => {
-
   //estado de los datos de las propiedades
-  const [data, setData] = useState<PropiedadInterface>();
+  const [data, setData] = useState<PropiedadInterface | undefined>(() => {
+    const storedData = localStorage.getItem("detailData");
+    return storedData ? JSON.parse(storedData) : undefined;
+  });
+
+  useEffect(() => {
+    if (data) {
+      localStorage.setItem("detailData", JSON.stringify(data));
+    }
+  }, [data]);
 
   return (
     <DetailContext.Provider value={{ data, setData }}>
