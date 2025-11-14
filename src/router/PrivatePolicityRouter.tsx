c5271b1688adb2974f08382cfc1af0f0d52a6d37
AndresBerdu <andres700crac@gmail.com>
2025-11-13 22:40:02 -0500

    se modificaron las rutas privadas

--- src/router/PrivatePolicityRouter.tsx
+++ src/router/PrivatePolicityRouter.tsx
@@ -1,13 +1,20 @@
 import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../firebase/hooks/useAuth";
+import { LoadingModal } from "../ui/modals/LoadingModal";
 
 export const PrivatePolicityRouter = ({
   children,
 }: {
   children: ReactNode;
 }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <LoadingModal/>
+    );
+  }
 
   return user ? children : <Navigate to="/" replace />;
 };
